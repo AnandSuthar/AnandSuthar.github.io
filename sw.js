e3a5bfd55786c5b8bb39c75ece4909a4a872498d
SunARcTech2 <sunarctech2@SunARcTech2s-Mac-mini.local>
2019-11-06 12:20:06 +0530

    changes

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-importScripts('/cache-polyfill.js');
-
-
-self.addEventListener('install', function(e) {
-                      e.waitUntil(
-                                  caches.open('airhorner').then(function(cache) {
-                                                                return cache.addAll([
-                                                                                     '/',
-                                                                                     '/index.html',
-                                                                                     '/index.html?homescreen=1',
-                                                                                     '/?homescreen=1',
-                                                                                     '/styles/main.css',
-                                                                                     '/scripts/main.min.js',
-                                                                                     '/sounds/airhorn.mp3'
-                                                                                     ]);
-                                                                })
-                                  );
-                      });
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+}
