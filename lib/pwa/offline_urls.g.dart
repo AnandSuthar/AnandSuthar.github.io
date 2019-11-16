dd3bb0f1a3868eb3d143656a485dd2ab19e4df86
SunARcTech2 <sunarctech2@SunARcTech2s-Mac-mini.local>
2019-11-16 12:27:21 +0530

    changes

--- lib/pwa/offline_urls.g.dart
+++
@@ -1,9 +0,0 @@
-/// URLs for offline cache.
-final List<String> offlineUrls = [
-  './flutter_assets/AssetManifest.json',
-  './flutter_assets/FontManifest.json',
-  './ios/Runner.build/Release-iphoneos/Runner.build/dgph',
-];
-
-/// Last modified timestamp of the files
-final String lastModified = '2019-11-14T12:33:52.330Z';
